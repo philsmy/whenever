de19da1eee3926f02fe961ba4d0cc9980907623e
Phil Smy <phil@philsmy.com>
2011-12-27 11:52:50 +0100

    I found that I needed bundle exec for runner

--- lib/whenever/setup.rb
+++ lib/whenever/setup.rb
@@ -18,7 +18,7 @@ end
 
 # Create a runner job that's appropriate for the Rails version,
 if Whenever.rails3?
-  job_type :runner, "cd :path && script/rails runner -e :environment ':task' :output"
+  job_type :runner, "cd :path && bundle exec script/rails runner -e :environment ':task' :output"
 else
   job_type :runner, "cd :path && script/runner -e :environment ':task' :output"
 end
